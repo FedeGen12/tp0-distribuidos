a0371d60fdfa400bd711735f7b6ca3cb6cbc26c7
Federico Genaro <fedegen14@gmail.com>
2025-09-01 17:26:21 -0300

    Agrego datos del cliente en envars

--- generar-compose.sh
+++ generar-compose.sh
@@ -31,6 +31,11 @@ do
     entrypoint: /client
     environment:
       - CLI_ID=$i
+      - NOMBRE=Santiago Lionel
+      - APELLIDO=Lorca
+      - DOCUMENTO=30904465
+      - NACIMIENTO=1999-03-17
+      - NUMERO=7574
     networks:
       - testing_net
     depends_on:
