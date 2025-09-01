1235750207611f8341385f9a022e6138f9a7453f
Federico Genaro <fedegen14@gmail.com>
2025-09-01 10:55:15 -0300

    Saco los loops del envío de mensajes

--- client/common/client.go
+++ client/common/client.go
@@ -1,15 +1,14 @@
 package common
 
 import (
-    "bufio"
-    "fmt"
-    "net"
-    "os"
-    "os/signal"
-    "syscall"
-    "time"
+	"bufio"
+	"fmt"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
 
-    "github.com/op/go-logging"
+	"github.com/op/go-logging"
 )
 
 var log = logging.MustGetLogger("log")
@@ -18,8 +17,6 @@ var log = logging.MustGetLogger("log")
 type ClientConfig struct {
 	ID            string
 	ServerAddress string
-	LoopAmount    int
-	LoopPeriod    time.Duration
 }
 
 // Client Entity that encapsulates how
@@ -53,51 +50,44 @@ func (c *Client) createClientSocket() error {
 	return nil
 }
 
-// StartClientLoop Send messages to the client until some time threshold is met
 func (c *Client) StartClientLoop() {
-	// There is an autoincremental msgID to identify every message sent
-	// Messages if the message amount threshold has not been surpassed
 	sigs := make(chan os.Signal, 1)
-    signal.Notify(sigs, syscall.SIGTERM)
+	signal.Notify(sigs, syscall.SIGTERM)
+	defer close(sigs)
 
-	for msgID := 1; msgID <= c.config.LoopAmount; msgID++ {
-		select {
-        case <-sigs:
-            log.Infof("action: shutdown | result: success | client_id: %v | msg: SIGTERM received", c.config.ID)
-            if c.conn != nil {
-                c.conn.Close()
-            }
-            return
-        default:
-            // Create the connection the server in every loop iteration. Send an
-            c.createClientSocket()
+	select {
+	case <-sigs:
+		log.Infof("action: shutdown | result: success | client_id: %v | msg: SIGTERM received", c.config.ID)
+		if c.conn != nil {
+			c.conn.Close()
+		}
+		return
+	default:
+		// Create the connection the server in every loop iteration. Send an
+		if c.createClientSocket() != nil {
+			return
+		}
+		defer c.conn.Close()
 
-            // TODO: Modify the send to avoid short-write
-            fmt.Fprintf(
-                c.conn,
-                "[CLIENT %v] Message N°%v\n",
-                c.config.ID,
-                msgID,
-            )
-            msg, err := bufio.NewReader(c.conn).ReadString('\n')
-            c.conn.Close()
+		// TODO: Modify the send to avoid short-write
+		fmt.Fprintf(
+			c.conn,
+			"[CLIENT %v] Message\n",
+			c.config.ID,
+		)
+		msg, err := bufio.NewReader(c.conn).ReadString('\n')
 
-            if err != nil {
-                log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
-                    c.config.ID,
-                    err,
-                )
-                return
-            }
+		if err != nil {
+			log.Errorf("action: receive_message | result: fail | client_id: %v | error: %v",
+				c.config.ID,
+				err,
+			)
+			return
+		}
 
-            log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
-                c.config.ID,
-                msg,
-            )
-
-            // Wait a time between sending one message and the next one
-            time.Sleep(c.config.LoopPeriod)
-        }
+		log.Infof("action: receive_message | result: success | client_id: %v | msg: %v",
+			c.config.ID,
+			msg,
+		)
 	}
-	log.Infof("action: loop_finished | result: success | client_id: %v", c.config.ID)
 }
