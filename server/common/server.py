67335cda5f75070cba4c5016c3e992a8b4ca179a
Federico Genaro <fedegen14@gmail.com>
2025-09-02 18:24:27 -0300

    Uso un lock para sincronizar el acceso al archivo de apuestas desde cada Process

--- server/common/server.py
+++ server/common/server.py
@@ -1,7 +1,6 @@
 import signal
 import logging
-from multiprocessing import Process
-
+from multiprocessing import Process, Lock
 from common.client_socket import ClientSocket, BATCH_MESSAGE, NOTIFY_MESSAGE
 from common.utils import store_bets, has_won, load_bets
 from common.server_socker import ServerSocket
@@ -22,6 +21,7 @@ class Server:
         self._running = True
         self._amount_clients = amount_clients
         self._server_socket = ServerSocket.setup_listener('', port, listen_backlog)
+        self._lock_bets_file = Lock()
 
         def sigterm_handler(_signum, _stacktrace):
             logging.info("action: shutdown | result: in_progress | msg: SIGTERM received")
@@ -43,12 +43,11 @@ class Server:
         while self._running and len(agencies) < self._amount_clients:
             try:
                 client_sock, client_id = self.__accept_new_connection()
-                self.__handle_client_connection(client_sock)
 
                 agency = Process(
                     name=str(client_id),
                     target=self.__handle_client_connection,
-                    args=client_sock,
+                    args=(client_sock, self._lock_bets_file),
                 )
 
                 agencies.append((agency, client_sock))
@@ -65,7 +64,7 @@ class Server:
             agency.join()
             agency_socket.close()
 
-    def __handle_client_connection(self, client_sock):
+    def __handle_client_connection(self, client_sock, lock_bets_file):
         """
         Read message from a specific client socket and closes the socket
 
@@ -76,8 +75,9 @@ class Server:
             type_message, message = client_sock.recv()
 
             if type_message == BATCH_MESSAGE:
-                store_bets(message)
-                logging.info(f"action: apuesta_recibida | result: success | cantidad: {len(message)}")
+                with lock_bets_file:
+                    store_bets(message)
+                    logging.info(f"action: apuesta_recibida | result: success | cantidad: {len(message)}")
 
             elif type_message == NOTIFY_MESSAGE:
                 logging.info(f"action: notificacion_recibida | result: success")
