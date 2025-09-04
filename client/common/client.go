c6664b5bf6ef8751d4557812f6b2bfe81816d417
Federico Genaro <fedegen14@gmail.com>
2025-09-04 14:16:19 -0300

    fix cierre graceful client y server

--- client/common/client.go
+++ client/common/client.go
@@ -2,9 +2,13 @@ package common
 
 import (
 	"encoding/csv"
+	"errors"
+	"fmt"
+	"net"
 	"os"
 	"os/signal"
 	"strconv"
+	"strings"
 	"syscall"
 
 	"github.com/op/go-logging"
@@ -57,50 +61,55 @@ func (c *Client) StartClientLoop(agencyFilePath string) {
 	signal.Notify(c.sigs, syscall.SIGTERM)
 	defer close(c.sigs)
 
-	select {
-	case <-c.sigs:
+	go func() {
+		<-c.sigs
 		c.sigtermHandler()
+	}()
+
+	if c.createClientSocket() != nil {
 		return
-	default:
-		if c.createClientSocket() != nil {
-			return
-		}
-		defer func(socket *ClientSocket) {
-			if c.socket != nil {
-				err := socket.Close()
-				if err != nil {
-					log.Errorf("action: close_socket | result: fail | client_id: %v | error: %v", c.config.ID, err)
-					return
-				} else {
-					log.Infof("action: close_socket | result: success | client_id: %v", c.config.ID)
-					c.socket = nil
-				}
-			}
-		}(c.socket)
-
-		c.sendClientId(c.config.ID)
-
-		agencyFile, err := os.Open(agencyFilePath)
-		if err != nil {
-			log.Criticalf("action: file_open | result: fail | client_id: %v | error: %v", c.config.ID, err)
-			return
-		}
-		defer func(agencyFile *os.File) {
-			closeErr := agencyFile.Close()
-			if closeErr != nil {
-				log.Criticalf("action: file_close | result: fail | client_id: %v | error: %v", c.config.ID, closeErr)
-				return
-			}
-		}(agencyFile)
-
-		err = c.sendBets(c.config.ID, agencyFile)
-		if err != nil {
-			return
-		}
-
-		c.sendNotifyMessage()
-		c.recvWinners()
 	}
+	defer func(socket *ClientSocket) {
+		if c.socket != nil {
+			err := socket.Close()
+			if err != nil {
+				log.Errorf("action: close_socket | result: fail | client_id: %v | error: %v", c.config.ID, err)
+				return
+			} else {
+				log.Infof("action: close_socket | result: success | client_id: %v", c.config.ID)
+				c.socket = nil
+			}
+		}
+	}(c.socket)
+
+	errId := c.sendClientId(c.config.ID)
+	if errId != nil {
+		return
+	}
+
+	agencyFile, err := os.Open(agencyFilePath)
+	if err != nil {
+		log.Criticalf("action: file_open | result: fail | client_id: %v | error: %v", c.config.ID, err)
+		return
+	}
+	defer func(agencyFile *os.File) {
+		closeErr := agencyFile.Close()
+		if closeErr != nil {
+			log.Criticalf("action: file_close | result: fail | client_id: %v | error: %v", c.config.ID, closeErr)
+			return
+		}
+	}(agencyFile)
+
+	err = c.sendBets(c.config.ID, agencyFile)
+	if err != nil {
+		return
+	}
+
+	err = c.sendNotifyMessage()
+	if err != nil {
+		return
+	}
+	c.recvWinners()
 }
 
 func (c *Client) sendBets(agencyId string, agencyFile *os.File) error {
@@ -112,9 +121,13 @@ func (c *Client) sendBets(agencyId string, agencyFile *os.File) error {
 		betLine, readErr := fileReader.Read()
 		if readErr != nil {
 			if len(currentBatch) > 0 {
-				c.sendBatch(currentBatch)
+				err := c.sendBatch(currentBatch)
+				if err != nil {
+					return err
+				}
 			}
-			break
+			log.Infof("action: send_bets | result: success | client_id: %v", c.config.ID)
+			return nil
 		}
 
 		currentBet := BetMessage{
@@ -133,7 +146,10 @@ func (c *Client) sendBets(agencyId string, agencyFile *os.File) error {
 		// Me fijo si entra con o sin el separador, porque puede ser la apuesta final del batch
 		if len(currentBatch) >= c.config.BatchMaxAmount ||
 			(currentBatchSize+betSize+SeparatorBetsSize > MaxBatchSizeBytes && currentBatchSize+betSize > MaxBatchSizeBytes) {
-			c.sendBatch(currentBatch)
+			err := c.sendBatch(currentBatch)
+			if err != nil {
+				return err
+			}
 			currentBatch = make([]BetMessage, 0)
 			currentBatchSize = 0
 		}
@@ -141,61 +157,64 @@ func (c *Client) sendBets(agencyId string, agencyFile *os.File) error {
 		currentBatch = append(currentBatch, currentBet)
 		currentBatchSize += betSize + SeparatorBetsSize
 	}
-
-	return nil
 }
 
-func (c *Client) sendBatch(batch []BetMessage) {
+func (c *Client) sendBatch(batch []BetMessage) error {
 	err := c.socket.Send(batch)
 	if err != nil {
-		log.Errorf("action: batch_enviado | result: fail | client_id: %v | error: %v", c.config.ID, err)
-		return
+		return fmt.Errorf("action: batch_enviado | result: fail | client_id: %v | error: %v", c.config.ID, err)
 	}
 	log.Infof("action: batch_enviado | result: success | client_id: %v", c.config.ID)
+	return nil
 }
 
 func (c *Client) sigtermHandler() {
 	log.Infof("action: shutdown | result: in_progress | client_id: %v | msg: SIGTERM received", c.config.ID)
 	if c.socket != nil {
 		err := c.socket.Close()
+		c.socket = nil
 		if err != nil {
 			log.Errorf("action: shutdown_close_socket | result: fail | client_id: %v | error: %v", c.config.ID, err)
 			log.Errorf("action: shutdown | result: fail | client_id: %v", c.config.ID)
 			return
 		} else {
 			log.Infof("action: shutdown_close_socket | result: success | client_id: %v", c.config.ID)
-			c.socket = nil
 		}
 	}
 	log.Infof("action: shutdown | result: success | client_id: %v", c.config.ID)
 	return
 }
 
-func (c *Client) sendNotifyMessage() {
+func (c *Client) sendNotifyMessage() error {
 	err := c.socket.SendNotifyMessage()
 	if err != nil {
-		log.Errorf("action: notify_message | result: fail | client_id: %v | error: %v", c.config.ID, err)
-		return
+		return fmt.Errorf("action: notify_message | result: fail | client_id: %v | error: %v", c.config.ID, err)
 	}
 	log.Infof("action: notify_message | result: success | client_id: %v", c.config.ID)
+	return nil
 }
 
-func (c *Client) sendClientId(clientId string) {
+func (c *Client) sendClientId(clientId string) error {
 	parsedClientId, parseErr := strconv.Atoi(clientId)
 	if parseErr != nil {
-		log.Errorf("action: parse_id | result: fail | client_id: %v | error: %v", clientId, parseErr)
+		return fmt.Errorf("action: parse_id | result: fail | client_id: %v | error: %v", clientId, parseErr)
 	}
 
 	err := c.socket.SendClientId(parsedClientId)
 	if err != nil {
-		log.Errorf("action: send_id | result: fail | client_id: %v | error: %v", clientId, err)
+		return fmt.Errorf("action: send_id | result: fail | client_id: %v | error: %v", clientId, err)
 	}
 	log.Infof("action: send_id | result: success | client_id: %v", clientId)
+	return nil
 }
 
 func (c *Client) recvWinners() {
 	winners, err := c.socket.RecvWinners()
 	if err != nil {
+		if errors.Is(err, net.ErrClosed) || strings.Contains(err.Error(), "use of closed network connection") {
+			log.Infof("action: consulta_ganadores | result: shutdown")
+			return
+		}
 		log.Errorf("action: consulta_ganadores | result: fail | error: %v", err)
 		return
 	}
--- server/common/server.py
+++ server/common/server.py
@@ -22,6 +22,7 @@ class Server:
         self._running = True
         self._amount_clients = amount_clients
         self._server_socket = ServerSocket.setup_listener('', port, listen_backlog)
+        self._agencies = {}
 
         def sigterm_handler(_signum, _stacktrace):
             logging.info("action: shutdown | result: in_progress | msg: SIGTERM received")
@@ -29,6 +30,7 @@ class Server:
             try:
                 self._server_socket.shutdown(socket.SHUT_RDWR)
                 logging.info("action: close_server_socket | result: success")
+                self.close_agency_sockets()
                 logging.info("action: shutdown | result: success")
             except OSError as e:
                 logging.error(f"action: close_server_socket | result: fail | error: {e}")
@@ -44,22 +46,27 @@ class Server:
         communication with a client. After client with communucation
         finishes, servers starts to accept new connections again
         """
-        agencies = {}
 
-        while self._running and len(agencies) < self._amount_clients:
+        while self._running and len(self._agencies) < self._amount_clients:
             try:
                 client_sock, client_id = self.__accept_new_connection()
                 self.__handle_client_connection(client_sock)
-                agencies[client_id] = client_sock
+                self._agencies[client_id] = client_sock
             except OSError:
                 # si se cerró el socket desde sigterm_handler → salir del loop
-                logging.info("action: shutdown del accept | result: success")
-                break
+                logging.info("action: shutdown del socket | result: success")
+                return
+
+        if not self._running:
+            logging.info("action: shutdown_server | result: success")
+            return
 
         self._server_socket.close()
-        self._get_winners(agencies)
+        self._get_winners()
+        self.close_agency_sockets()
 
-        for agency_id, agency_socket in agencies.items():
+    def close_agency_sockets(self):
+        for agency_id, agency_socket in self._agencies.items():
             try:
                 agency_socket.close()
                 logging.info(f"action: close_client_socket | result: success | client_id: {agency_id}")
@@ -84,13 +91,13 @@ class Server:
                 logging.info(f"action: notificacion_recibida | result: success")
                 break
 
-    def _get_winners(self, agencies):
+    def _get_winners(self):
         if self._running:
             logging.info("action: sorteo | result: success")
 
             agency_winners = obtain_winners()
 
-            for agency, agency_socket in agencies.items():
+            for agency, agency_socket in self._agencies.items():
                 agency_socket.send_winners(agency_winners[agency])
 
     def __accept_new_connection(self):
